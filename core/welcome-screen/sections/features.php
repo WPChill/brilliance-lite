28fdc3559db01e599e3a0c73ef9c8718f4065e9d
Raiber Cristian <cristian@machothemes.com>
2017-07-06 14:55:46 +0300

    Updated Theme Upsell Page
    - Added .gitignore
    - Updated utm tracking
    - Updated Upsell page

--- core/welcome-screen/sections/features.php
+++ core/welcome-screen/sections/features.php
@@ -7,28 +7,33 @@ if ( ! defined( 'WPINC' ) ) {
  */
 
 $features = array(
-	'slider-options' => array(
-		'label'   	=> __( 'Slider options', 'brilliance' ),
-		'cpo'     	=> '<span class="dashicons dashicons-no-alt"></span>',
-		'cpo-pro'	=> '<span class="dashicons dashicons-yes"></span></i>'
-	),
-	'woocommerce' => array(
-		'label'  	=> __( 'WooCommerce Integration', 'brilliance' ),
-		'cpo'     	=> '<span class="dashicons dashicons-no-alt"></span>',
-		'cpo-pro' 	=> '<span class="dashicons dashicons-yes"></span></i>'
-	),
-	'reorder-sections' => array(
-		'label'       => __( 'Reorder Homepage Sections', 'brilliance' ),
+	'slider-options'    => array(
+		'label'   => esc_html__( 'Improved Slider Options', 'brilliance' ),
+		'sub'     => esc_html__( 'Add more slides, control the appearance & position of slides.', 'brilliance' ),
 		'cpo'     => '<span class="dashicons dashicons-no-alt"></span>',
 		'cpo-pro' => '<span class="dashicons dashicons-yes"></span></i>'
 	),
-	'custom-colors'    => array(
-		'label'       => __( 'Custom Colors', 'brilliance' ),
+	'woocommerce'       => array(
+		'label'   => esc_html__( 'WooCommerce Support', 'brilliance' ),
+		'sub'     => esc_html__( 'Create a WooCommerce powered shop. Supports WooCommerce v3.x and upwards', 'brilliance' ),
 		'cpo'     => '<span class="dashicons dashicons-no-alt"></span>',
 		'cpo-pro' => '<span class="dashicons dashicons-yes"></span></i>'
 	),
-	'typography'       => array(
-		'label'       => __( 'Custom Typography', 'brilliance' ),
+	'reorder-sections'  => array(
+		'label'   => esc_html__( 'Reorder Homepage Sections', 'brilliance' ),
+		'sub'     => esc_html__( 'Re-order your site\'s front-page sections in any way you want.', 'brilliance' ),
+		'cpo'     => '<span class="dashicons dashicons-no-alt"></span>',
+		'cpo-pro' => '<span class="dashicons dashicons-yes"></span></i>'
+	),
+	'custom-colors'     => array(
+		'label'   => esc_html__( 'Custom Color Schemes & Color Controls', 'brilliance' ),
+		'sub'     => esc_html__( 'Easily change your site\'s color schemes.', 'brilliance' ),
+		'cpo'     => '<span class="dashicons dashicons-no-alt"></span>',
+		'cpo-pro' => '<span class="dashicons dashicons-yes"></span></i>'
+	),
+	'typography'        => array(
+		'label'   => esc_html__( 'Custom Typography Controls', 'brilliance' ),
+		'sub'     => esc_html__( 'Want a different font family? No problem. Just an upgrade away.', 'brilliance' ),
 		'cpo'     => '<span class="dashicons dashicons-no-alt"></span>',
 		'cpo-pro' => '<span class="dashicons dashicons-yes"></span></i>'
 	),
@@ -56,11 +61,10 @@ $features = array(
         <tbody>
 		<?php foreach ( $features as $feature ): ?>
             <tr>
-                <td class="feature">
-                    <h3>
-						<?php echo $feature['label']; ?>
-                    </h3>
-                </td>
+	            <td class="feature">
+		            <h3><?php echo $feature['label']; ?></h3>
+		            <p><?php echo $feature['sub']; ?></p>
+	            </td>
                 <td class="cpo-feature">
 					<?php echo $feature['cpo']; ?>
                 </td>
@@ -71,8 +75,8 @@ $features = array(
 		<?php endforeach; ?>
         <tr>
             <td></td>
-            <td colspan="2" class="text-right"><a href="//www.cpothemes.com/theme/brilliance?utm_source=upsell&utm_medium=theme&utm_campaign=upsell" target="_blank"
-                               class="button button-primary button-hero"><span class="dashicons dashicons-cart"></span><?php _e( 'Get Pro Now!', 'brilliance' ) ?></a></td>
+            <td colspan="2" class="text-right"><a href="//www.cpothemes.com/theme/brilliance?utm_source=worg&utm_medium=theme&utm_campaign=upsell" target="_blank"
+                               class="button button-primary button-hero"><span class="dashicons dashicons-cart"></span><?php _e( 'Get the Pro Version Now!', 'brilliance' ) ?></a></td>
         </tr>
         </tbody>
     </table>
