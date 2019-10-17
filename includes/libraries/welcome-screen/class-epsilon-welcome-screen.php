7a2fb5dafe5cb04a8862d548cd783a4af2c5fbdd
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-10-17 17:20:12 +0300

    removed nopriv ajax action

--- includes/libraries/welcome-screen/class-epsilon-welcome-screen.php
+++ includes/libraries/welcome-screen/class-epsilon-welcome-screen.php
@@ -157,13 +157,7 @@ class Epsilon_Welcome_Screen {
 				'welcome_screen_ajax_callback',
 			)
 		);
-		add_action(
-			'wp_ajax_nopriv_welcome_screen_ajax_callback', array(
-				$this,
-				'welcome_screen_ajax_callback',
-			)
-		);
-
+		
 		if ( $this->edd ) {
 			/**
 			 * Initiate EDD Stuff
